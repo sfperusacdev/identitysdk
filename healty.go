a2a672139e492b6901f9dcddbbf8c1755d9097b1
Kevin Saucedo <developer2@sfperusac.com>
2023-05-22 15:41:50 -0500

    add identity server health checker

---
+++ healty.go
@@ -0,0 +1,39 @@
+package identitysdk
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func IdentityServerCheckHealth() error {
+	hostUrl, err := url.JoinPath(identityAddress, "/health")
+	if err != nil {
+		if logger != nil {
+			logger.Error(err.Error())
+		}
+		return err
+	}
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, hostUrl, nil)
+	if err != nil {
+		if logger != nil {
+			logger.Error(err.Error())
+		}
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if logger != nil {
+			logger.Error(err.Error())
+		}
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		payload, _ := io.ReadAll(res.Body)
+		return fmt.Errorf("identity server error: %s", string(payload))
+	}
+	return nil
+}
