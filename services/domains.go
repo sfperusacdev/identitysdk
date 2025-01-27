223cf0a78ac2562e3381c21309ff0657cd13e723
Kevin Sacuedo <user002_@outlook.com>
2025-01-27 10:39:16 -0500

    add GetEmpresas on ExternalBridgeService

--- services/domains.go
+++ services/domains.go
@@ -19,3 +19,22 @@ func (s *ExternalBridgeService) GetDominios(ctx context.Context) ([]string, erro
 	}
 	return apiresponse.Data, nil
 }
+
+type Empresa struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+func (s *ExternalBridgeService) GetEmpresas(ctx context.Context) ([]Empresa, error) {
+	var apiresponse struct {
+		Message string    `json:"message"`
+		Data    []Empresa `json:"data"`
+	}
+	var err = s.makeRequest(ctx,
+		identitysdk.GetIdentityServer(),
+		"/v1/get-list-empresas-full", "-", &apiresponse)
+	if err != nil {
+		return nil, err
+	}
+	return apiresponse.Data, nil
+}
