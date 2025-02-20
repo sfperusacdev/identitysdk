a3ddf6480cc3190dafce0c65e310bf5f48afeefa
Kevin Sacuedo <user002_@outlook.com>
2025-02-20 16:21:48 -0500

    add file storer aws

--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ toolchain go1.23.1
 
 require (
 	github.com/allegro/bigcache/v3 v3.1.0
+	github.com/aws/aws-sdk-go-v2 v1.36.2
+	github.com/aws/aws-sdk-go-v2/config v1.29.7
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.77.1
 	github.com/jinzhu/copier v0.4.0
 	github.com/labstack/echo/v4 v4.13.3
 	github.com/stretchr/testify v1.10.0
@@ -16,6 +19,21 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.60 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.29 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.33 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.33 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.33 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.6.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.16 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.15 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
---
+++ helpers/file_store.go
@@ -0,0 +1,81 @@
+package services
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"path"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3FileStore struct {
+	bucketName   string
+	subdirectory string
+	s3Client     *s3.Client
+}
+
+func NewS3FileStore(ctx context.Context, awsRegion, bucketBasePath string) (*S3FileStore, error) {
+	conf, err := config.LoadDefaultConfig(ctx, config.WithRegion(awsRegion))
+	if err != nil {
+		return nil, err
+	}
+	var parts = strings.Split(bucketBasePath, "/")
+	return &S3FileStore{
+		bucketName:   parts[0],
+		s3Client:     s3.NewFromConfig(conf),
+		subdirectory: strings.Join(parts[1:], "/"),
+	}, nil
+}
+
+func (s *S3FileStore) getFullPath(filepath string) string {
+	return path.Join(s.subdirectory, filepath)
+}
+
+func (s *S3FileStore) Read(ctx context.Context, filepath string) []byte {
+	fullPath := s.getFullPath(filepath)
+	obj, err := s.s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(s.bucketName),
+		Key:    aws.String(fullPath),
+	})
+	if err != nil {
+		return nil
+	}
+	defer obj.Body.Close()
+
+	buf := new(bytes.Buffer)
+	if _, err := io.Copy(buf, obj.Body); err != nil {
+		return nil
+	}
+
+	return buf.Bytes()
+}
+
+func (s *S3FileStore) Save(ctx context.Context, path string, data []byte) error {
+	fullPath := s.getFullPath(path)
+	_, err := s.s3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(s.bucketName),
+		Key:    aws.String(fullPath),
+		Body:   bytes.NewReader(data),
+	})
+	return err
+}
+
+func (s *S3FileStore) Delete(ctx context.Context, filepath string) error {
+	fullPath := s.getFullPath(filepath)
+	_, err := s.s3Client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(s.bucketName),
+		Key:    aws.String(fullPath),
+	})
+	return err
+}
+
+func (s *S3FileStore) Replace(ctx context.Context, path string, data []byte) error {
+	if err := s.Delete(ctx, path); err != nil {
+		return err
+	}
+	return s.Save(ctx, path, data)
+}
