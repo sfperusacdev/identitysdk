dd301b442c9c624c2d0c4ca444cc5fb27a6ed0e8
Kevin Sacuedo <user002_@outlook.com>
2025-05-06 18:00:24 -0500

    add setup package

--- go.mod
+++ go.mod
@@ -13,11 +13,16 @@ require (
 	github.com/jinzhu/copier v0.4.0
 	github.com/labstack/echo/v4 v4.13.3
 	github.com/panjf2000/ants/v2 v2.11.3
+	github.com/pressly/goose/v3 v3.24.2
 	github.com/resend/resend-go/v2 v2.18.0
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/viper v1.20.1
 	github.com/stretchr/testify v1.10.0
 	github.com/user0608/goones v0.10.0
 	github.com/user0608/ifdevmode v0.0.3
+	github.com/user0608/pg-connection v1.2.0
 	go.uber.org/fx v1.23.0
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
@@ -36,23 +41,40 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.15 // indirect
 	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.2 // indirect
+	github.com/jackc/pgx/v5 v5.7.4 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sagikazarmark/locafero v0.9.0 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.14.0 // indirect
+	github.com/spf13/cast v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/dig v1.18.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
-	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sync v0.13.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/time v0.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/postgres v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
---
+++ setup/setup.go
@@ -0,0 +1,185 @@
+package setup
+
+import (
+	"context"
+	"database/sql"
+	"embed"
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"path"
+
+	"github.com/pressly/goose/v3"
+	"github.com/sfperusacdev/identitysdk/httpapi"
+	"github.com/spf13/cobra"
+	connection "github.com/user0608/pg-connection"
+	"go.uber.org/fx"
+	"gopkg.in/yaml.v2"
+)
+
+type Service struct {
+	configPath     *ConfigPath
+	migrationsDir  embed.FS
+	version        string
+	Command        *cobra.Command
+	configProvider ConfigsProviderFunc
+}
+
+func NewService(
+	version string,
+	migrationsDir embed.FS,
+) *Service {
+	service := &Service{
+		version:        version,
+		migrationsDir:  migrationsDir,
+		configProvider: DefaultConfigsProviderFunc,
+	}
+	service.Command = &cobra.Command{
+		Use:  path.Base(os.Args[0]),
+		Args: service.prepareConfigPath,
+	}
+
+	service.Command.PersistentFlags().StringP("config", "c", "", "location of the application's configuration file")
+	service.Command.PersistentFlags().BoolP("auto", "a", false, "auto migrate database")
+
+	service.Command.AddCommand(
+		&cobra.Command{
+			Use: "version", Short: "Print the version number",
+			Run: func(cmd *cobra.Command, args []string) { fmt.Println(version) },
+		},
+		&cobra.Command{
+			Use:   "config-example",
+			Short: "Generates a base YAML configuration template for the application (config.yml)",
+			Run: func(cmd *cobra.Command, args []string) {
+				data, err := yaml.Marshal(&GeneralServiceConfig{})
+				if err != nil {
+					slog.Error("Failed to marsahl yml", "error", err)
+					os.Exit(1)
+				}
+				fmt.Println(string(data))
+			},
+		},
+		service.migrationCommand("upgrade", "Upgrade the database schema to the latest version", "up"),
+		service.migrationCommand("downgrade", "Downgrade the database schema to a previous version", "down"),
+		service.migrationCommand("status", "Show database version status", "status"),
+	)
+	return service
+}
+
+func (s *Service) SetConfigsProviderFunc(provider ConfigsProviderFunc) { s.configProvider = provider }
+
+func (s *Service) prepareConfigPath(cmd *cobra.Command, args []string) error {
+	configPath := ConfigPath(cmd.Flag("config").Value.String())
+	if configPath == "" {
+		return errors.New("the --config file path was not provided or is empty")
+	}
+	s.configPath = &configPath
+	return nil
+}
+
+func (s *Service) configs() (GeneralServiceConfigProvider, DatabaseConfigProvider, error) {
+	if s.configPath == nil {
+		slog.Error("configPath is nil")
+		return nil, nil, errors.New("config path is nil")
+	}
+
+	ceneralConfig, dbconfig, err := s.configProvider(*s.configPath)
+	if err != nil {
+		slog.Error("Error fetching database configuration", "error", err)
+		return nil, nil, err
+	}
+	return ceneralConfig, dbconfig, nil
+}
+
+func (s *Service) getCconnectionManager() (connection.StorageManager, error) {
+	_, c, err := s.configs()
+	if err != nil {
+		slog.Error("Error fetching database configs", "error", err)
+		return nil, err
+	}
+
+	return connection.OpenWithConfigs(connection.DBConfigParams{
+		DBHost:     c.GetHost(),
+		DBPort:     fmt.Sprint(c.GetPort()),
+		DBName:     c.GetDBName(),
+		DBUsername: c.GetUsername(),
+		DBPassword: c.GetPassword(),
+		DBLogLevel: c.GetLogLevel(),
+	})
+
+}
+func (s *Service) getDatabaseConnection() (*sql.DB, error) {
+	connectionManager, err := s.getCconnectionManager()
+	if err != nil {
+		slog.Error("Error opening database connection", "error", err)
+		return nil, err
+	}
+
+	conn := connectionManager.Conn(context.Background())
+	db, err := conn.DB()
+	if err != nil {
+		slog.Error("Error recovering database connection", "error", err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (s *Service) migrationCommand(use, shortDesc, migrationType string) *cobra.Command {
+	return &cobra.Command{
+		Use:   use,
+		Short: shortDesc,
+		Args:  s.prepareConfigPath,
+		Run: func(cmd *cobra.Command, args []string) {
+
+			db, err := s.getDatabaseConnection()
+			if err != nil {
+				slog.Error("Failed to establish database connection", "error", err)
+				os.Exit(1)
+			}
+			goose.SetBaseFS(s.migrationsDir)
+			err = goose.RunWithOptionsContext(context.Background(), migrationType, db, "migrations", []string{})
+			if err != nil {
+				slog.Error(fmt.Sprintf("Database %s failed", migrationType), "error", err)
+				os.Exit(1)
+			}
+		},
+	}
+}
+
+func (s *Service) Run(opts ...fx.Option) error {
+	s.Command.Run = func(cmd *cobra.Command, args []string) {
+		connectionManager, err := s.getCconnectionManager()
+		if err != nil {
+			slog.Error("Error opening database connection", "error", err)
+			os.Exit(1)
+		}
+		gsc, _, err := s.configs()
+		if err != nil {
+			slog.Error("Error loading configs", "error", err)
+			os.Exit(1)
+		}
+
+		opts = append(
+			opts,
+			fx.Provide(
+				func() GeneralServiceConfigProvider { return gsc },
+				func() connection.StorageManager { return connectionManager },
+				func(c GeneralServiceConfigProvider) httpapi.ServeURLString {
+					return httpapi.ServeURLString(c.ListenAddress())
+				},
+			),
+			httpapi.Module,
+			fx.Invoke(httpapi.StartWebServer),
+		)
+		app := fx.New(opts...)
+		app.Run()
+		slog.Info("Application stopped")
+	}
+	if err := s.Command.Execute(); err != nil {
+		slog.Error("command execution failed", "error", err)
+		return err
+	}
+	return nil
+}
