f5de5b67aaff8e70cbfb4be469240e0701ed7298
Kevin Sacuedo <user002_@outlook.com>
2025-05-09 10:09:40 -0500

    add TaskRunner

---
+++ helpers/taskrunner/taskrunner.go
@@ -0,0 +1,63 @@
+package taskrunner
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type TaskRunner struct {
+	ctx       context.Context
+	cancel    context.CancelFunc
+	wg        sync.WaitGroup
+	isRunning atomic.Bool
+	task      func()
+	interval  time.Duration
+}
+
+func NewTaskRunner(task func(), interval time.Duration) *TaskRunner {
+	return &TaskRunner{
+		task:     task,
+		interval: interval,
+	}
+}
+
+func (tr *TaskRunner) Start() {
+	if tr.isRunning.Load() {
+		return
+	}
+
+	tr.isRunning.Store(true)
+	tr.ctx, tr.cancel = context.WithCancel(context.Background())
+	tr.wg.Add(1)
+
+	go func() {
+		defer func() {
+			tr.isRunning.Store(false)
+			tr.wg.Done()
+		}()
+		for {
+			select {
+			case <-tr.ctx.Done():
+				return
+			default:
+				tr.task()
+				time.Sleep(tr.interval)
+			}
+		}
+	}()
+}
+
+func (tr *TaskRunner) Stop() {
+	if !tr.isRunning.Load() {
+		return
+	}
+
+	tr.cancel()
+	tr.wg.Wait()
+}
+
+func (tr *TaskRunner) IsRunning() bool {
+	return tr.isRunning.Load()
+}
---
+++ helpers/taskrunner/taskrunner_test.go
@@ -0,0 +1,70 @@
+package taskrunner_test
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/sfperusacdev/identitysdk/helpers/taskrunner"
+)
+
+func TestConcurrentStartStop(t *testing.T) {
+
+	task := func() {
+		fmt.Println("Executing task at", time.Now().Format(time.RFC3339))
+	}
+
+	runner := taskrunner.NewTaskRunner(task, 2*time.Second)
+
+	var wg sync.WaitGroup
+
+	for range 10 {
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			runner.Start()
+		}()
+
+		go func() {
+			defer wg.Done()
+			time.Sleep(500 * time.Millisecond)
+			runner.Stop()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func TestTaskExecution(t *testing.T) {
+	var taskCount int64
+	expectedExecutions := 5
+	interval := 500 * time.Millisecond
+	totalWaitTime := time.Duration(expectedExecutions) * interval
+
+	task := func() {
+		atomic.AddInt64(&taskCount, 1)
+	}
+
+	runner := taskrunner.NewTaskRunner(task, interval)
+
+	startTime := time.Now()
+	runner.Start()
+	time.Sleep(totalWaitTime)
+	runner.Stop()
+	elapsedTime := time.Since(startTime)
+
+	executions := atomic.LoadInt64(&taskCount)
+
+	if executions < int64(expectedExecutions) {
+		t.Fatalf("Expected at least %d executions, but got %d", expectedExecutions, executions)
+	}
+
+	if elapsedTime < totalWaitTime {
+		t.Fatalf("Expected at least %v elapsed, but got %v", totalWaitTime, elapsedTime)
+	}
+
+	t.Logf("Task executed %d times in %v", executions, elapsedTime)
+}
